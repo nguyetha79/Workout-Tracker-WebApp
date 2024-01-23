4ef90fd2ed0bdc9ea73ddffa3e02f0d4e6911ad4
nguyetha79 <nguyet-ha1.phung@stu.uni-regensburg.de>
2024-01-23 10:11:41 +0100

    add file

---
+++ backend/models/WorkoutModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    sets: {
+      type: Number,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+    restPeriods: {
+      type: String,
+      required: true,
+    },
+    tempo: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    user_id: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Workout", workoutSchema);
